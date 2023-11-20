e3d191a87c51ff6d664be42373839b27a4f05c1b
Joanna Sowa <104635556+josovva@users.noreply.github.com>
2023-11-20 18:14:24 +0100

    Add files via upload

---
+++ TAIO/Graph.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAIO
+{
+    public class Graph
+    {
+        public int[,] AdjustmentMatrix { get; private set; }
+        public int NumberOfVertices => AdjustmentMatrix.GetLength(0);
+        public Graph(int[,] adjustmentMatrix) => AdjustmentMatrix = adjustmentMatrix;
+        public int GetWeight(int v, int u) => AdjustmentMatrix[v, u];
+
+        public void AddEdge(int v, int u) => AdjustmentMatrix[v, u]++;
+
+        public void RemoveEdge(int v, int u)
+        {
+            if (AdjustmentMatrix[v, u] > 0)
+                AdjustmentMatrix[v, u]--;
+        }
+
+        public void AddVertices(int k)
+        {
+            if (k > 0)
+            {
+                int[,] newAdjustmentMatrix = new int[AdjustmentMatrix.GetLength(0) + k, AdjustmentMatrix.GetLength(0) + k];
+
+                for (int i = 0; i < AdjustmentMatrix.GetLength(0); i++)
+                    for (int j = 0; j < AdjustmentMatrix.GetLength(0); j++)
+                        newAdjustmentMatrix[i, j] = AdjustmentMatrix[i, j];
+
+                AdjustmentMatrix = newAdjustmentMatrix;
+            }
+        }
+    }
+}

1b835d7a6cc6d23a5df2636c2e5dd9ce8e23923f
Jakub Urbański <kuba.urban01@gmail.com>
2023-12-01 16:30:07 +0100

    Fix main

--- TAIO/Program.cs
+++ TAIO/Program.cs
@@ -101,40 +101,12 @@ int[,] B =
 
 #region Maximum clique
 
-//var l = 1;
-//var p = 0.5;
+var l = 1;
+var p = 0.5;
 
-//(var porosity, var clique) = MaxClique.Calculate(new IntermediateGraph(A), l, p, (genome) => genome.NumberOfVertices);
+(var porosity, var clique) = MaxClique.Calculate(new IntermediateGraph(A), l, p, (genome) => genome.NumberOfVertices);
 
-//var g = new Graph(A);
-//g.DisplaySolution(clique, l, porosity);
+var g = new Graph(A);
+g.DisplaySolution(clique, l, porosity);
 
 #endregion
-
-string fileName = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "..", "asia_test.txt");
-
-try
-{
-    Graph[] graphs = Parser.ParseFile(fileName);
-
-    foreach (var graph in graphs)
-    {
-        Console.WriteLine($"Liczba wierzchołków: {graph.NumberOfVertices}");
-        Console.WriteLine("Macierz sąsiedztwa:");
-
-        for (int i = 0; i < graph.NumberOfVertices; i++)
-        {
-            for (int j = 0; j < graph.NumberOfVertices; j++)
-            {
-                Console.Write($"{graph.AdjustmentMatrix[i, j]} ");
-            }
-            Console.WriteLine();
-        }
-
-        Console.WriteLine();
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Wystąpił błąd: {ex.Message}");
-}

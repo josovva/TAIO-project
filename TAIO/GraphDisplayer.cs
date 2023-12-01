01d8e5e9797954508a3bd7bebfa5a96563ca7f2a
Jakub Urbański <kuba.urban01@gmail.com>
2023-12-01 13:54:12 +0100

    Fix graph displayer for >=100 numbers

--- TAIO/GraphDisplayer.cs
+++ TAIO/GraphDisplayer.cs
@@ -11,30 +11,32 @@ public static class GraphDisplayer
 
     public static void DisplaySolution(this Graph g, List<int> solutionVertices)
     {
+        Console.WriteLine();
+        
         Console.ForegroundColor = DEFAULT_COLOR;
 
-        Console.Write($"     | ");
+        Console.Write($"      | ");
         for (int i = 0; i < g.NumberOfVertices; i++)
         {
             if (solutionVertices.Contains(i))
             {
                 Console.ForegroundColor = SOLUTION_VERTEX_COLOR;
 
-                Console.Write(i < 10 ? $" {i} " : $"{i} ");
+                Console.Write($"{i}".PadLeft(5));
 
                 Console.ForegroundColor = DEFAULT_COLOR;
             }
             else
             {
-                Console.Write(i < 10 ? $" {i} " : $"{i} ");
+                Console.Write($"{i}".PadLeft(5));
             }
         }
         Console.WriteLine();
 
-        Console.Write($"  {UNDERLINE}   |");
+        Console.Write($"  {UNDERLINE}    | ");
         for (int i = 0; i < g.NumberOfVertices; i++)
         {
-            Console.Write("   ");
+            Console.Write("     ");
         }
         Console.WriteLine(RESET_UNDERLINE);
 
@@ -42,17 +44,19 @@ public static class GraphDisplayer
 
         for (int i = 0; i < g.NumberOfVertices; i++)
         {
+            Console.WriteLine($"      |");
+
             if (solutionVertices.Contains(i))
             {
                 Console.ForegroundColor = SOLUTION_VERTEX_COLOR;
 
-                Console.Write(i < 10 ? $"  {i}" : $" {i}");
+                Console.Write($"{i}".PadLeft(4));
 
                 Console.ForegroundColor = DEFAULT_COLOR;
             }
             else
             {
-                Console.Write(i < 10 ? $"  {i}" : $" {i}");
+                Console.Write($"{i}".PadLeft(4));
             }
 
             Console.Write("  | ");
@@ -62,19 +66,19 @@ public static class GraphDisplayer
                 var weight = g.GetWeight(i, j);
                 if (weight == 0)
                 {
-                    Console.Write($"   ");
+                    Console.Write($"     ");
                 }
                 else if (solutionVertices.Contains(i) && solutionVertices.Contains(j))
                 {
                     Console.ForegroundColor = SOLUTION_EDGE_COLOR;
 
-                    Console.Write(weight < 10 ? $" {weight} " : $"{weight} ");
+                    Console.Write($"{weight}".PadLeft(5));
 
                     Console.ForegroundColor = DEFAULT_COLOR;
                 }
                 else
                 {
-                    Console.Write(weight < 10 ? $" {weight} " : $"{weight} ");
+                    Console.Write($"{weight}".PadLeft(5));
                 }
             }
             Console.WriteLine();
